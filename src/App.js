d704433c8a5d55a538b0ad3b768f44c2ccbe9f09
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-06-24 23:29:27 +0100

    Add 'growing paths'

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ import Toolbar from './components/toolbar/Toolbar';
 import getCanvasCenter from './actions/draw/getCanvasCenter';
 import getConnectedPoints from './actions/api/getConnectedPoints';
 
-let callTrack = 0;
+// Not used
+const globals = {
+  fps: 60,
+  callTrack: 0,
+};
 
 export default function App() {
-  console.log(`App is called ${(callTrack += 1)} time(s)`);
+  console.log(`App is called ${(globals.callTrack += 1)} time(s)`);
 
   const canvasRef = React.useRef(null);
   const paperScopeRef = React.useRef(new paper.PaperScope());
+  const pathRef = React.useRef(null);
+
+  const [coordinates, setCoordinates] = React.useState([]);
+  const [coordinatesDisp, setCoordinatesDisp] = React.useState([]);
+  const [isPlaying, setIsPlaying] = React.useState(false);
 
   // Set up Paper.js and the canvas element.
   // Note: this effect only runs once on component mount and once on component
@@ -35,18 +44,63 @@ export default function App() {
 
   const add = async () => {
     const canvas = canvasRef.current;
+    const paperScope = paperScopeRef.current;
 
+    // Get data from NOOP's API
     const centerPoint = new paper.Point(getCanvasCenter(canvas));
-    const noopConnectedPoints = await getConnectedPoints(centerPoint, 1000);
+    const noopConnectedPoints = await getConnectedPoints(centerPoint, 100);
 
-    const p = new paper.Path({
-      segments: noopConnectedPoints,
+    setCoordinates(
+      noopConnectedPoints.map(point => ({ x: point.x, y: point.y }))
+    );
+    setCoordinatesDisp([]);
+
+    // Need to re-active the PaperScope after setState() hook calls.
+    // Why? Don't know yet...
+    paperScope.activate();
+    pathRef.current = new paper.Path({
+      segments: [],
       strokeColor: 'black',
     });
   };
 
+  const togglePlaying = () => {
+    const view = paperScopeRef.current.view;
+
+    if (isPlaying) {
+      view.onFrame = null;
+      setIsPlaying(false);
+    } else {
+      view.onFrame = e => {
+        const path = pathRef.current;
+
+        if (coordinatesDisp.length !== coordinates.length && path !== null) {
+          const i = coordinatesDisp.length;
+          const pointToAdd = new paper.Point(coordinates[i]);
+
+          path.add(pointToAdd);
+
+          coordinatesDisp.push(coordinates[i]);
+        } else {
+          view.onFrame = null;
+          setIsPlaying(false);
+        }
+      };
+      setIsPlaying(true);
+    }
+  };
+
+  const sayInfo = () => {
+    console.log(
+      `Current view's size: ${paperScopeRef.current.view.size.width}` +
+        ` / ${paperScopeRef.current.view.size.height}`
+    );
+  };
+
   const allActions = {
     add,
+    togglePlaying,
+    sayInfo,
   };
 
   return (
@@ -59,7 +113,7 @@ export default function App() {
       >
         An HTML canvas.
       </canvas>
-      <Toolbar allActions={allActions} />
+      <Toolbar allActions={allActions} isPlaying={isPlaying} />
     </div>
   );
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -2,16 +2,33 @@ import React from 'react';
 import styles from './toolbar.module.css';
 import Button from './Button';
 import { ReactComponent as AddCircleOutline } from '../../assets/icons/AddCircleOutline.svg';
+import { ReactComponent as InfoOutline } from '../../assets/icons/InfoOutline.svg';
+import { ReactComponent as PlayCircleOutline } from '../../assets/icons/PlayCircleOutline.svg';
+import { ReactComponent as PauseCircleOutline } from '../../assets/icons/PauseCircleOutline.svg';
 
-export default function Toolbar({ allActions }) {
+export default function Toolbar({ allActions, isPlaying }) {
   const handleAdd = () => {
     allActions.add();
+  };
+
+  const handleSayInfo = () => {
+    allActions.sayInfo();
   };
 
   return (
     <div className={styles.toolbar}>
       <Button onClick={handleAdd}>
         <AddCircleOutline className={styles.buttonIcon} />
+      </Button>
+      <Button onClick={allActions.togglePlaying}>
+        {isPlaying ? (
+          <PauseCircleOutline className={styles.buttonIcon} />
+        ) : (
+          <PlayCircleOutline className={styles.buttonIcon} />
+        )}
+      </Button>
+      <Button onClick={handleSayInfo}>
+        <InfoOutline className={styles.buttonIcon} />
       </Button>
     </div>
   );
